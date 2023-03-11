6df8675bb050c33b4c77039ba2e1f295d7693b6d
Yuran Simao <yuransimao7@gmail.com>
2023-03-11 16:41:03 +0100

    update Clients

--- src/Core/Clients/Client.ts
+++ src/Core/Clients/Client.ts
@@ -1,3 +1,24 @@
 export default class Clients {
-    
+    #id : string
+    #name : string
+    #age : number 
+    constructor(name: string, age: number, id : string = null){
+        this.#id = id
+        this.#name = name
+        this.#age = age 
+    }
+
+    static vazio(){
+        return new Clients('', 0)
+    }
+
+    get id (){
+        return this.#id
+    }
+    get name (){
+        return this.#name
+    }
+    get age (){
+        return this.#age
+    }
 } 
