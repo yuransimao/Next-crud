868d130830dba5fe90f8c72c1c927254ced54d07
Yuran Simao <yuransimao7@gmail.com>
2023-03-11 21:53:43 +0100

    update form

--- src/Components/Formulario/inputs.tsx
+++ src/Components/Formulario/inputs.tsx
@@ -1,11 +1,14 @@
 interface Inputprops{
-
+    type: 'text' | 'number'
+    text: string
+    value: number
 }
 
 export default function Inputs(props : Inputprops){
     return(
         <div>
-
+            <label>{props.text}</label>
+            <input type={`${props.type ?? 'text'}`}/>
         </div>
     )
 }
