7971a7704d3e34df21dfaa6c6ffc7a254934b938
Yuran Simao <yuransimao7@gmail.com>
2023-03-11 16:24:57 +0100

    adding Clients

--- src/Components/Layout/Layout.tsx
+++ src/Components/Layout/Layout.tsx
@@ -8,7 +8,7 @@ export default function Layout(props: propsLayout){
     <div className={`flex flex-col w-2/3 bg-white text-gray-800 rounded-md`}>
         <Title>{props.title}</Title>
 
-        <div>
+        <div className='p-6'>
             {props.children}
         </div>
     </div>
