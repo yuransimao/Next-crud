8b74483413f257d201f7b1e3b85b9d4253deb049
Yuran Simao <yuransimao7@gmail.com>
2023-03-11 16:16:37 +0100

    adding Title

--- src/Components/Layout/Layout.tsx
+++ src/Components/Layout/Layout.tsx
@@ -5,7 +5,7 @@ interface propsLayout{
 }
 export default function Layout(props: propsLayout){
     return(
-    <div className={`flex flex-col w-2/3 bg-white text-gray-800`}>
+    <div className={`flex flex-col w-2/3 bg-white text-gray-800 rounded-md`}>
         <Title>{props.title}</Title>
 
         <div>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,7 +5,9 @@ export default function Home() {
 
   return (
     <div className={`flex justify-center items-center h-screen bg-gradient-to-r from-blue-500 to-purple-500 text-white`}>
-      <Layout title={'Cadastro Simples'}/>
+      <Layout title={'Cadastro Simples'}>
+        <span>Content</span>
+      </Layout>
     </div>
   )
 }
