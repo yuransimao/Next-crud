1b3c6c4bdb7ca915b9955570baa1fb08886cc09a
Yuran Simao <yuransimao7@gmail.com>
2023-03-11 15:38:38 +0100

    adding components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,8 +4,8 @@ export default function Home() {
  
 
   return (
-    <div className=''>
-    
+    <div className={`flex justify-center items-center h-screen bg-gradient-to-r from-blue-500 to-purple-500 text-white`}>
+      <h1>App</h1>
     </div>
   )
 }
