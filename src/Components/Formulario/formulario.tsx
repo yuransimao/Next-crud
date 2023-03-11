4294526b43fdb92e70f26cef986470620f300db1
Yuran Simao <yuransimao7@gmail.com>
2023-03-11 23:17:36 +0100

    update and adding button save and change

--- src/Components/Formulario/formulario.tsx
+++ src/Components/Formulario/formulario.tsx
@@ -1,12 +1,31 @@
+import Clients from "../../Core/Clients/Client"
 import Inputs from "./inputs"
+import Button from '../Button/button'
+import {useState} from "react"
 interface FormularioProps{
-
+    Clients?: Clients
 }
 
 export default function Formulario(props : FormularioProps){
+    const id = props.Clients?.id
+    const [name, setName] = useState( props.Clients?.name ?? '')
+    const [Age, setAge] = useState( props.Clients?.age ?? '')
     return(
         <div>
-        <Inputs text='Name' value='text' />
+        {id ? (<Inputs ClassName='mb-5' text='Codigo' value={id} somenteLeitura/> ):false}
+
+        <Inputs text='Name' ClassName='mb-5' value={name} valorMudou={setName} />
+        <Inputs text='Age' type='number' value={Age} valorMudou={setAge} />
+
+        <div className={'flex justify-end mt-3'}>
+
+            <Button cor='blue' ClassName='mr-2'>
+                {id ? 'Altera' : 'Salvar'}
+            </Button>
+            <Button cor='gray'>
+                Cancelar
+            </Button>
+        </div>
         </div>
     )
 }
--- src/Components/Formulario/inputs.tsx
+++ src/Components/Formulario/inputs.tsx
@@ -3,14 +3,18 @@ interface Inputprops{
     text: string
     value: any
     somenteLeitura?:boolean
+    ClassName?: string
+    valorMudou?: (value:any) => void
 }
 
 export default function Inputs(props : Inputprops){
     return(
-        <div className={`flex flex-col`}>
-            <label>{props.text}</label>
+        <div className={`flex flex-col ${props.ClassName}`}>
+
+            <label className='mb-2'>{props.text}</label>
             <input type={props.type ?? 'text'} value={props.value} readOnly={props.somenteLeitura}
-            className={'border border-purple-500 rounded-lg focus:outline-none'}
+            onChange={e => props.valorMudou(e.target.value)}
+            className={`border border-purple-500 rounded-lg focus:outline-none bg-gray-100 px-4 py-2 ${props.somenteLeitura ? ' ' : 'focus:bg-white'}`}
             />
         </div>
     )
