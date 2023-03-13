be38ebf5b8d2281b78f3d0762197c3ef31ecafe5
Yuran Simao <yuransimao7@gmail.com>
2023-03-13 20:49:47 +0100

    update

--- src/Components/Formulario/inputFile.tsx
+++ src/Components/Formulario/inputFile.tsx
@@ -1,8 +1,21 @@
-
-
+import { Camera } from "../Icons/icons"
+import {  useRef} from "react";
 
 export default function InputFile(){
+    const input = useRef<HTMLInputElement>()
+    function haldleFile(){
+        console.log('test')
+    }
     return(
-        <div className={`flex justify-end `}><input type="file" accept="image/*"/></div>
+       
+        <div className={`flex justify-end `}>
+            <div className={`flex flex-col`}>
+                <div className={`w-14 h-14 rounded-full bg-slate-600 ` }> 
+                <img className={`w-14 h-14 rounded-full` }src={`${Camera}`} alt="camera" />
+                </div>
+                <input ref={input} className="hidden" type="file" accept="image/*"/>
+                <button className={`absolute mt-8 ml-9 text-sky-700`} onAuxClick={haldleFile}>{Camera}</button>
+            </div>
+            </div>
     )
 }
